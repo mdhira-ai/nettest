3283a8d7cd1d160971e06b9c9667129ff8a91870
mdhira_linux <mdhera211@gmail.com>
2025-02-27 21:58:59 -0500

    v2

--- main.cpp
+++ main.cpp
@@ -7,15 +7,29 @@ using json = nlohmann::json;
 int main(int, char **)
 {
 
-    std::string vl;
+    std::string version;
+    std::string changelog;
+    std::string downloadurl;
+
     std::cout << "Hello, please check update!\n";
 
-    cpr::Response r = cpr::Get(cpr::Url{"https://httpbin.org/get"});
+    cpr::Response r = cpr::Get(cpr::Url{"https://raw.githubusercontent.com/mdhira-ai/nettest/refs/heads/master/update.json"});
 
     json jsondata = json::parse(r.text);
-    vl = jsondata.at("origin");
+    version = jsondata.at("version");
+    changelog = jsondata.at("changelog");
+    downloadurl = jsondata.at("downloadurl");
 
-    std::cout << vl << std::endl;
+    auto currentversion = "1.0.0";
+
+    if (currentversion == version)
+    {
+        std::cout << "no update";
+    }
+    else
+    {
+        std::cout << "there is new update available " << version << std::endl;
+    }
 
     return 0;
 }
